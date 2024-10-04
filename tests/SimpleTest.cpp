b5ab4878066f580af25507b7057268a2f0f47202
DXXN <pexwp@mail.ru>
2024-10-04 14:36:16 +0300

    1 simple test added

--- tests/SimpleTest.cpp
+++ tests/SimpleTest.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 #include "../main.cpp"
+#include <cstddef>
+
 
 TEST(SimpleTest, foo) {
-	RUN_ALL_TESTS();
+	std::byte x{ 97 };
+	std::byte y{ 67 };
+	EXPECT_EQ(
+		y,
+		foo(x)
+	);
 }
