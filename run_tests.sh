3d217a70fe08c4af0b3d1fc79ff9e7055204eea0
DXXN <pexwp@mail.ru>
2024-10-05 11:45:36 +0300

    fixed wrong dir path in run tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,2 @@
-if test -d build/release/tests/release; then
-	cd build/release/tests/Release;
-	./SimpleTest
-else
-	cd build/release/tests/Debug;
-	./SimpleTest
-fi
+cd build/release/tests;
+./SimpleTest
