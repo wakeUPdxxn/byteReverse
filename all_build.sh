210d3adcb1bc93e298f4d7a34b824bb12474ad98
DXXN <119891660+wakeUPdxxn@users.noreply.github.com>
2024-10-07 02:42:10 +0300

    Dev (#8)
    * false test added and one simple
    
    * some code rework + windows compatibility T types header
    
    * cmake file wrong target link libs fixed
    
    * pulling cmake fixes from dev (#2)
    
    * Update pipeline.yml
    
    * Create automerge.yml
    
    * Update automerge.yml
    
    * Dev (#1)
    
    * false test added and one simple
    
    * some code rework + windows compatibility T types header
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Revert "pulling cmake fixes from dev (#2)" (#3)
    
    This reverts commit 399471a80ca3c19511d3c108da28c278876127ef.
    
    * Revert "pulling cmake fixes from dev (#2)" (#4)
    
    This reverts commit 399471a80ca3c19511d3c108da28c278876127ef.
    
    * set cmake compiler
    
    * revert cmake file changes
    
    * workflow file changed
    
    * extra namespace removed
    
    * msvc detect - abort removed,scripts modified

---
+++ all_build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [[ "$OSTYPE" == "msys" ]]; 
+then
+   cmake -S . -B build/release -G "MinGW Makefiles" -D CMAKE_BUILD_TYPE=Release
+   cmake --build build/release'
+else
+   cmake -S . -B build/release -D CMAKE_BUILD_TYPE=Release
+   cmake --build build/release'
+fi
+
